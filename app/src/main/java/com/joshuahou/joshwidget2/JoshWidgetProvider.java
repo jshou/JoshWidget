65a8a36bcb9157b9cf6b27b8c815b7bb3aa8d329
jshou <joshuahou@gmail.com>
2016-03-11 18:41:25 -0800

    works but it crashes

---
+++ app/src/main/java/com/joshuahou/joshwidget2/JoshWidgetProvider.java
@@ -0,0 +1,17 @@
+package com.joshuahou.joshwidget2;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class JoshWidgetProvider extends BroadcastReceiver {
+    public JoshWidgetProvider() {
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // TODO: This method is called when the BroadcastReceiver is receiving
+        // an Intent broadcast.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
