840479f122bec3231c3fd20225317772e8285361
jshou <joshuahou@gmail.com>
2016-03-11 18:42:29 -0800

    don't crash

--- app/src/main/java/com/joshuahou/joshwidget2/JoshWidgetProvider.java
+++ app/src/main/java/com/joshuahou/joshwidget2/JoshWidgetProvider.java
@@ -12,6 +12,5 @@ public class JoshWidgetProvider extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         // TODO: This method is called when the BroadcastReceiver is receiving
         // an Intent broadcast.
-        throw new UnsupportedOperationException("Not yet implemented");
     }
 }
